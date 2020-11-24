b2f8b4a77bedf79b411695ffdc017231c3278263
сергей <sergey_fedorov200001@mail.ru>
2020-11-24 21:57:14 +0300

    Advanced Bot created (fixes)

--- src/AdvancedBot.kt
+++ src/AdvancedBot.kt
@@ -1,13 +1,48 @@
+import java.util.concurrent.ThreadLocalRandom
+
 /**
  * Данный бот использует алгоритм оценки позиций для выбора своего хода
+ * Просчитывает все возможные ходы на данном этапе и выбирает наилучший
  */
 
 class AdvancedBot : Player {
     override fun selectMove(board: Board): Cells {
 
-        //Используем класс оценки позиций
-        val evaluatePos = EvaluatePosition()
-        return evaluatePos.evaluatePosition(board)
+        var validMoves = board.getValidMoves(board.isWhiteTurn())
+
+        if (validMoves.isEmpty())
+            throw IllegalStateException("Невозможная ситуация")
+
+        if (validMoves.size == 1)
+            return validMoves.first()
+
+        //Лист в котором будут храниться клетки валидных ходов с их оценкой
+        val listOfEvaluatePosition = mutableListOf<Pair<Cells, Double>>()
+
+        while (validMoves.isNotEmpty()) {
+            val cell = validMoves.first()
+            val evaluatePos = EvaluatePosition()
+
+            listOfEvaluatePosition.add(Pair(cell, evaluatePos.evaluatePosition(board,cell)))
+
+            validMoves = validMoves.drop(1).toSet()
+        }
+
+        //Если получится несколько клеток с одинаковым и наибольшим значением -> из них выберем рандомную
+        //Отсортируем список по убыванию
+        var resultList = listOfEvaluatePosition.sortedBy { it.second }.reversed()
+
+        //Получим максимальное значение и отфильтруем список, чтобы остались только одинаковые и равные наибольшему
+        val maxValue = resultList.first().second
+        resultList = listOfEvaluatePosition.filter { it.second == maxValue }
+
+        if (resultList.size == 1)
+            return resultList.first().first
+
+        //Выберем из них рандомное
+        val index = ThreadLocalRandom.current().nextInt(0, resultList.size - 1)
+        return resultList.toList()[index].first
+
     }
 
 }
--- src/EvaluatePosition.kt
+++ src/EvaluatePosition.kt
@@ -1,8 +1,10 @@
-import java.util.concurrent.ThreadLocalRandom
+/*
+Цифры для оценки позиций и конечной формулы взяты с сайта: https://cutt.ly/XhpmrWl
+ */
 
 class EvaluatePosition {
 
-    private val evaluativeMap = Array(8) {IntArray(8) {0} }
+    private val evaluativeMap = Array(8) { IntArray(8) { 0 } }
 
     init {
         //Проинициализируем матрицу
@@ -17,196 +19,166 @@ class EvaluatePosition {
     }
 
     //Функция оценки позиций
-    fun evaluatePosition(board: Board): Cells {
-
-        var validMoves = board.getValidMoves(board.isWhiteTurn())
-
-        if (validMoves.isEmpty())
-            throw IllegalStateException("Невозможная ситуация")
-
-        if (validMoves.size == 1)
-            return validMoves.first()
-
-        //Лист в котором будут храниться клетки валидных ходов с их оценкой
-        val listOfEvaluatePosition = mutableListOf<Pair<Cells, Double>>()
+    fun evaluatePosition(board: Board, cell: Cells): Double {
 
         //если true -> я хожу за белых, иначе за черных
         val whatColorIsMine = board.isWhiteTurn()
 
+        val newBoard = board.makeCopyAndMove(cell) //Делаем ход на копии доски
 
-        while (validMoves.isNotEmpty()) {
+        val emptyCells = newBoard.getEmptyCells()
+        val myCells: Set<Cells> //клетки с фишками нашего цвета
+        val enemyCells: Set<Cells> //клетки с фишками соперника
 
-            val cell = validMoves.first()
-            val newBoard = board.makeCopyAndMove(cell)
+        //В зависимости от того, кто сейчас ходит myCells и enemyCells присваиваются по разному
+        if (whatColorIsMine) {
+            myCells = newBoard.getWhite()
+            enemyCells = newBoard.getBlack()
+        } else {
+            myCells = newBoard.getBlack()
+            enemyCells = newBoard.getWhite()
+        }
 
-            val emptyCells = newBoard.getEmptyCells()
-            var myCells: Set<Cells> //клетки с фишками нашего цвета
-            var enemyCells: Set<Cells> //клетки с фишками соперника
+        //Введем параметры для конечной формулы
+        val whoHasMoreCells = (myCells.size - enemyCells.size).toDouble() // У кого сейчас больше фишек
+        val cellsSizeParameter: Double // количество фишек
+        val frontCells: Double // параметр для каревых фишек(с одной из сторон есть пустая клетка)
+        val cornerCells: Double // параметр для угловых фишек
+        val nearCornerCells: Double // параметр для клеток рядом с углами доски
+        val mobility: Double // параметр мобильности игроков (у кого после нашего хода будет больше валидных ходов)
 
-            //В зависимости от того, кто сейчас ходит myCells и enemyCells присваиваются по разному
-            if (whatColorIsMine) {
-                myCells = newBoard.getWhite()
-                enemyCells = newBoard.getBlack()
-            } else {
-                myCells = newBoard.getBlack()
-                enemyCells = newBoard.getWhite()
-            }
+        //Доп функция для вычисления параметров myFrontCellsSize и enemyFrontCellsSize
+        fun findFrontCellsSize(whichCells: Set<Cells>): Int {
 
-            //Введем параметры для конечной формулы
-            val d = (myCells.size - enemyCells.size).toDouble()
-            var p: Double
-            var f: Double
-            var c: Double
-            var l: Double
-            var m: Double
+            var x: Int
+            var y: Int
+            val listOfX = intArrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
+            val listOfY = intArrayOf(0, 1, 1, 1, 0, -1, -1, -1)
 
-            fun findFrontCellsSize(whichCells: Set<Cells>) : Int {
+            var result = 0
 
-                var x: Int
-                var y: Int
-                val listOfX = intArrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
-                val listOfY = intArrayOf(0, 1, 1, 1, 0, -1, -1, -1)
+            whichCells.forEach {
 
-                var result = 0
+                for (k in 0..7) {
+                    x = it.h + listOfX[k]
+                    y = it.v + listOfY[k]
 
-                whichCells.forEach {
-
-                    for (k in 0..7) {
-                        x = it.h + listOfX[k]
-                        y = it.v + listOfY[k]
-
-                        if (x in 0..7 && y in 0..7 && Cells.fromCell(x, y) in emptyCells) {
-                            result++
-                            break
-                        }
-
+                    if (x in 0..7 && y in 0..7 && Cells.fromCell(x, y) in emptyCells) {
+                        result++
+                        break
                     }
 
                 }
-                return result
+
             }
-
-            val myFrontCellsSize = findFrontCellsSize(myCells)
-            val enemyFrontCellsSize = findFrontCellsSize(enemyCells)
-
-            var myCellsSize = myCells.size
-            var enemyCellsSize = enemyCells.size
-
-            p = when {
-                myCellsSize > enemyCellsSize -> (100.0 * myCellsSize) / (myCellsSize + enemyCellsSize)
-                myCellsSize < enemyCellsSize -> -(100.0 * enemyCellsSize) / (myCellsSize + enemyCellsSize)
-                else -> 0.0
-            }
-
-            f = when {
-                myFrontCellsSize > enemyFrontCellsSize -> -(100.0 * myFrontCellsSize)/(myFrontCellsSize + enemyFrontCellsSize)
-                myFrontCellsSize < enemyFrontCellsSize -> (100.0 * enemyFrontCellsSize)/(myFrontCellsSize + enemyFrontCellsSize)
-                else -> 0.0
-            }
-
-            //Размещение фишек в угловых клетках
-            myCellsSize = 0
-            enemyCellsSize = 0
-            when{
-                Cells.A1 in myCells -> myCellsSize++
-                Cells.A1 in enemyCells -> enemyCellsSize++
-
-                Cells.H1 in myCells -> myCellsSize++
-                Cells.H1 in enemyCells -> enemyCellsSize++
-
-                Cells.A8 in myCells -> myCellsSize++
-                Cells.A8 in enemyCells -> enemyCellsSize++
-
-                Cells.H8 in myCells -> myCellsSize++
-                Cells.H8 in enemyCells -> enemyCellsSize++
-            }
-
-            c = 25 * (myCellsSize - enemyCellsSize).toDouble()
-
-            //Размещение фишек в близких к углам клетках
-            myCellsSize = 0
-            enemyCellsSize = 0
-            if (Cells.A1 in emptyCells) {
-                when{
-                    Cells.A2 in myCells -> myCellsSize++
-                    Cells.A2 in enemyCells -> enemyCellsSize++
-
-                    Cells.B1 in myCells -> myCellsSize++
-                    Cells.B1 in enemyCells -> enemyCellsSize++
-
-                    Cells.B2 in myCells -> myCellsSize++
-                    Cells.B2 in enemyCells -> enemyCellsSize++
-                }
-            }
-
-            if (Cells.H1 in emptyCells) {
-                when{
-                    Cells.H2 in myCells -> myCellsSize++
-                    Cells.H2 in enemyCells -> enemyCellsSize++
-
-                    Cells.G1 in myCells -> myCellsSize++
-                    Cells.G1 in enemyCells -> enemyCellsSize++
-
-                    Cells.G2 in myCells -> myCellsSize++
-                    Cells.G2 in enemyCells -> enemyCellsSize++
-                }
-            }
-
-            if (Cells.A8 in emptyCells) {
-                when{
-                    Cells.A7 in myCells -> myCellsSize++
-                    Cells.A7 in enemyCells -> enemyCellsSize++
-
-                    Cells.B8 in myCells -> myCellsSize++
-                    Cells.B8 in enemyCells -> enemyCellsSize++
-
-                    Cells.B7 in myCells -> myCellsSize++
-                    Cells.B7 in enemyCells -> enemyCellsSize++
-                }
-            }
-
-            if ( Cells.H8 in emptyCells) {
-                when{
-                    Cells.H7 in myCells -> myCellsSize++
-                    Cells.H7 in enemyCells -> enemyCellsSize++
-
-                    Cells.G7 in myCells -> myCellsSize++
-                    Cells.G7 in enemyCells -> enemyCellsSize++
-
-                    Cells.G8 in myCells -> myCellsSize++
-                    Cells.G8 in enemyCells -> enemyCellsSize++
-                }
-            }
-
-            l = -12.5 * (myCellsSize - enemyCellsSize)
-
-            //Мобильность игроков
-            myCellsSize = newBoard.getValidMoves(whatColorIsMine).size
-            enemyCellsSize = newBoard.getValidMoves(!whatColorIsMine).size
-            m = when {
-                myCellsSize > enemyCellsSize -> (100.0 * myCellsSize)/(myCellsSize + enemyCellsSize)
-                myCellsSize < enemyCellsSize -> -(100.0 * enemyCellsSize)/(myCellsSize + enemyCellsSize)
-                else -> 0.0
-            }
-
-            val score = (10 * p) + (801.724 * c) + (382.026 * l) + (78.922 * m) + (74.396 * f) + (10 * d)
-            listOfEvaluatePosition.add(Pair(cell, score))
-
-            validMoves = validMoves.drop(1).toSet()
+            return result
         }
 
-        //Если получится несколько клеток с одинаковым и наибольшим значением -> из них выберем рандомную
-        var resultList = listOfEvaluatePosition.sortedBy { it.second }.reversed()
+        val myFrontCellsSize = findFrontCellsSize(myCells)
+        val enemyFrontCellsSize = findFrontCellsSize(enemyCells)
 
-        val maxValue = resultList.first().second
-        resultList = listOfEvaluatePosition.filter { it.second == maxValue }
+        var myCellsSize = myCells.size
+        var enemyCellsSize = enemyCells.size
 
-        if (resultList.size == 1)
-            return resultList.first().first
+        cellsSizeParameter = when {
+            myCellsSize > enemyCellsSize -> (100.0 * myCellsSize) / (myCellsSize + enemyCellsSize)
+            myCellsSize < enemyCellsSize -> -(100.0 * enemyCellsSize) / (myCellsSize + enemyCellsSize)
+            else -> 0.0
+        }
 
-        val index = ThreadLocalRandom.current().nextInt(0, resultList.size - 1)
-        return resultList.toList()[index].first
+        frontCells = when {
+            myFrontCellsSize > enemyFrontCellsSize -> -(100.0 * myFrontCellsSize) / (myFrontCellsSize + enemyFrontCellsSize)
+            myFrontCellsSize < enemyFrontCellsSize -> (100.0 * enemyFrontCellsSize) / (myFrontCellsSize + enemyFrontCellsSize)
+            else -> 0.0
+        }
+
+        //Размещение фишек в угловых клетках
+        myCellsSize = 0
+        enemyCellsSize = 0
+        when {
+            Cells.A1 in myCells -> myCellsSize++
+            Cells.A1 in enemyCells -> enemyCellsSize++
+
+            Cells.H1 in myCells -> myCellsSize++
+            Cells.H1 in enemyCells -> enemyCellsSize++
+
+            Cells.A8 in myCells -> myCellsSize++
+            Cells.A8 in enemyCells -> enemyCellsSize++
+
+            Cells.H8 in myCells -> myCellsSize++
+            Cells.H8 in enemyCells -> enemyCellsSize++
+        }
+
+        cornerCells = 25 * (myCellsSize - enemyCellsSize).toDouble()
+
+        //Размещение фишек в близких к углам клетках
+        myCellsSize = 0
+        enemyCellsSize = 0
+        if (Cells.A1 in emptyCells) {
+            when {
+                Cells.A2 in myCells -> myCellsSize++
+                Cells.A2 in enemyCells -> enemyCellsSize++
+
+                Cells.B1 in myCells -> myCellsSize++
+                Cells.B1 in enemyCells -> enemyCellsSize++
+
+                Cells.B2 in myCells -> myCellsSize++
+                Cells.B2 in enemyCells -> enemyCellsSize++
+            }
+        }
+
+        if (Cells.H1 in emptyCells) {
+            when {
+                Cells.H2 in myCells -> myCellsSize++
+                Cells.H2 in enemyCells -> enemyCellsSize++
+
+                Cells.G1 in myCells -> myCellsSize++
+                Cells.G1 in enemyCells -> enemyCellsSize++
+
+                Cells.G2 in myCells -> myCellsSize++
+                Cells.G2 in enemyCells -> enemyCellsSize++
+            }
+        }
+
+        if (Cells.A8 in emptyCells) {
+            when {
+                Cells.A7 in myCells -> myCellsSize++
+                Cells.A7 in enemyCells -> enemyCellsSize++
+
+                Cells.B8 in myCells -> myCellsSize++
+                Cells.B8 in enemyCells -> enemyCellsSize++
+
+                Cells.B7 in myCells -> myCellsSize++
+                Cells.B7 in enemyCells -> enemyCellsSize++
+            }
+        }
+
+        if (Cells.H8 in emptyCells) {
+            when {
+                Cells.H7 in myCells -> myCellsSize++
+                Cells.H7 in enemyCells -> enemyCellsSize++
+
+                Cells.G7 in myCells -> myCellsSize++
+                Cells.G7 in enemyCells -> enemyCellsSize++
+
+                Cells.G8 in myCells -> myCellsSize++
+                Cells.G8 in enemyCells -> enemyCellsSize++
+            }
+        }
+
+        nearCornerCells = -12.5 * (myCellsSize - enemyCellsSize)
+
+        //Мобильность игроков
+        myCellsSize = newBoard.getValidMoves(whatColorIsMine).size
+        enemyCellsSize = newBoard.getValidMoves(!whatColorIsMine).size
+        mobility = when {
+            myCellsSize > enemyCellsSize -> (100.0 * myCellsSize) / (myCellsSize + enemyCellsSize)
+            myCellsSize < enemyCellsSize -> -(100.0 * enemyCellsSize) / (myCellsSize + enemyCellsSize)
+            else -> 0.0
+        }
+
+        return (10 * cellsSizeParameter) + (801.724 * cornerCells) + (382.026 * nearCornerCells) +
+                (78.922 * mobility) + (74.396 * frontCells) + (10 * whoHasMoreCells)
 
     }
-
 }
